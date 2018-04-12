af7c026a056fdf629726cea8ca234a8bd05d63da
Chaos-Sheeza <jesmar16@live.com>
2018-04-12 23:28:36 +0200

    Created a literals class and allowed user to enter clauses

---
+++ Task1.py
@@ -0,0 +1,19 @@
+# Object of type clause
+class clause():
+    def __init__(self):
+        self.literals = []
+
+    def add(self, literal):
+        self.literals.append(literal)
+
+
+user, arr = [], []
+while user != ["end"]:
+    test = clause()
+    user = raw_input("Enter literals\n").split(", ")
+    if user != ["end"]:
+        test.add(user)
+        arr.append(test)
+
+for i in range(len(arr)):
+    print(arr[i].literals)

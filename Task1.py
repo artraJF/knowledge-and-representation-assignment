6e1f47b931c1ad2f2376d4d92642d293f0908dd8
Chaos-Sheeza <jesmar16@live.com>
2018-05-03 22:29:57 +0200

    Removed redundant code, added comments, and edited user input

--- Task1.py
+++ Task1.py
@@ -1,5 +1,6 @@
 # Create a class for knowledge base
 class KnowledgeBase:
+    # Initialises knowledge base
     def __init__(self):
         self.clauses = []
 
@@ -12,104 +13,73 @@ class KnowledgeBase:
         for i in self.clauses:
             print(i)
 
-
-# Limitation: could only accept a query with one literal
-'''
-def backchain(KnBs, q):
-    # Once query is empty then the query is true
-    if len(q) == 0:
-        return True
-    else:
-        check = False
-        # Switches negated query into positive query to search and vice versa
-        if "!" in q[0]:
-            q[0] = q[0].replace("!", "")
+    # Using same algorithm as before to accept more than 1 literal
+    def backchain(self, q):
+        # If query is empty then query entails knowledge base
+        if len(q) == 0:
+            return True
         else:
-            q[0] = "!" + q[0]
-        # Searches the KB for q, if found then the new query is the found clause without the result
-        # If not found then the function returns false
-        for i in KnBs.clauses:
-            if q[0] in i:
-                # Prints the opposite of the query and in which clause it is found
-                print("Found %s in %s" % (q, i))
-                i.remove(q[0])
-                q = i
-                check = True
-                break
-        if not check:
-            return False
-        elif check:
-            # If the clause found has more than one literal, queries each literal to see if all are true
-            # If all are true, returns true, else returns false
-            check = True
-            for i in range(len(q)):
-                if not backchain(KnBs, [q[i]]):
-                    check = False
-            if not check:
-                return False
-            elif check:
-                return True
-'''
-
-
-# Using same algorithm as before to accept more than 1 literal
-def backchain(KB, q):
-    if len(q) == 0:
-        return True
-    else:
-        check = False
-        if len(q) == 1:
-            if "!" in q[0]:
-                q[0] = q[0].replace("!", "")
-            else:
-                q[0] = "!" + q[0]
-            for i in KB.clauses:
-                if q[0] in i:
-                    print("Found %s in %s" % (q, i))
-                    i.remove(q[0])
-                    q = i
-                    check = True
-                    break
-            if not check:
-                return False
-            elif check:
-                if backchain(KB, q):
-                    return True
-                elif not backchain(KB, q):
+            check = False       # Variable used later to check whether a matching literal is found
+            # If query only contains 1 literal, its polarity is changed, then searches the KB to find a matching literal
+            # If a literal is found check is set to true,
+            # and the new query is the clause found minus the matching literal
+            # Later, if check is true the query continues on, else the current method returns false
+            if len(q) == 1:
+                if "!" in q[0]:
+                    q[0] = q[0].replace("!", "")
+                else:
+                    q[0] = "!" + q[0]
+                for i in KB.clauses:
+                    if q[0] in i:
+                        print("Found %s in %s" % (q, i))
+                        i.remove(q[0])
+                        q = i
+                        check = True
+                        break
+                if not check:
                     return False
-        else:
-            check = True
-            for i in range(len(q)):
-                if not backchain(KB,[q[i]]):
-                    check = False
-                    break
-            if check:
-                return True
-            elif not check:
-                return False
+                elif check:
+                    if self.backchain(q):
+                        return True
+                    elif not self.backchain(q):
+                        return False
+            # If query has multiple literals then the method checks whether all literals are in the knowledge base,
+            # splits clause into singular literals a recursively calls the method again
+            else:
+                check = True
+                for i in range(len(q)):
+                    if not self.backchain([q[i]]):
+                        check = False
+                        break
+                if check:
+                    return True
+                elif not check:
+                    return False
 
 
+# Main method that allows user input
 KB = KnowledgeBase()
 user = ""
+print("Enter clauses line by line. Use ! for negation and type end (case sensitive) to close the knowledge base.")
+print("Make sure that literals are separated with a , \nExample: !Mammal, Human")
 # Loop that adds clauses to the knowledge base
 while user != ["end"]:
-    user = raw_input("Enter clauses in knowledge base (type end to exit)\n").split(", ")
+    user = raw_input("Enter a clause in knowledge base\n").split(", ")
     if user == ["end"]:
         break
     KB.add(user)
-# Variable tat stores user input
+# Variable that stores user input
 query = [raw_input("Enter your negated (!) query\n")]
 # Temporary storage for output
 temp = query
-res = backchain(KB, query)
+res = KB.backchain(query)
 if res:
     print("SOLVED \nKB |= %s" % temp)
 elif not res:
     print("NOT SOLVED \nKB does not |= %s" % temp)
 
-
+# Static input for testing
 '''
-# Testing worked
 KB = KnowledgeBase()
 KB.add(['FirstGrade'])
 KB.add(['!FirstGrade', 'Child'])
@@ -120,7 +90,7 @@ KB.add(['Female'])
 KB.add(['Male'])
 query = ['!Boy', '!Girl']
 store = query
-test = backchain(KB, query)
+test = KB.backchain(query)
 if test:
     print("SOLVED \nKB |= %s" % store)
 elif not test:
